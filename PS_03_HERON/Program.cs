f9d76d0240e14196b30450efe8a219ba71ecb1f8
mikechris-sv <Sheilamaeveluya@gmail.com>
2024-09-20 23:05:19 +0800

    added heron

---
+++ PS_03_HERON/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace PS_03_HERON
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first side of a triangle: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter second side of a triangle: ");
+            double b = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter third side of a triangle: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+
+            
+            HeronFormula(a,b,c);
+            Console.ReadKey();
+
+        }
+        
+        public static void HeronFormula(double a, double b, double c)
+        {
+            double s = (a + b + c) / 2;
+
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+
+            Console.WriteLine($"The area of the three sides of a triangle is: {area:F2}");
+        }
+    }
+}
--- PS_03_LEAPYEAR/obj/Release/net8.0/PS_03_LEAPYEAR.AssemblyInfo.cs
+++ PS_03_LEAPYEAR/obj/Release/net8.0/PS_03_LEAPYEAR.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PS_03_LEAPYEAR")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f51a33c3ef927539ecc81bf1c86ae64994bedede")]
 [assembly: System.Reflection.AssemblyProductAttribute("PS_03_LEAPYEAR")]
