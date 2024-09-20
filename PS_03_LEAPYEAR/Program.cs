f51a33c3ef927539ecc81bf1c86ae64994bedede
mikechris-sv <Sheilamaeveluya@gmail.com>
2024-09-20 22:20:10 +0800

    end

--- PS_03_LEAPYEAR/Program.cs
+++ PS_03_LEAPYEAR/Program.cs
@@ -4,8 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            // close lang visual studio everytime na mag commit or push
+            Console.Write("Enter a year: ");
+            int year = int.Parse(Console.ReadLine());
+
+            bool isLeapYear = LeapYear(year);
+            Console.WriteLine($"Does {year} year a leap year? {isLeapYear} ");
+
+        }
+        public static bool LeapYear(int year)
+        {
+            if (year % 4 == 0)
+            {
+                if (year % 100 == 0)
+                {
+                    return year % 400 == 0;
+                }
+                return true;
+            }
+            return false;
         }
     }
 }
